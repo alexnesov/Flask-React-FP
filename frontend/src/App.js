4336035f18e469c8aa0b8631760777d5fd63bba5
alexnesov <alexandrenesovic@gmail.com>
2022-04-17 16:26:09 +0200

    gts front init

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,37 +1,15 @@
-import { useState, useEffect } from 'react'
-import axios from 'axios'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const [date, setDate] = useState('March 20, 2022')
 
-  useEffect(() => {
-    async function fetchDate() {
-      const resp = await axios({
-        url: '/api/date',
-        method: 'get'
-      })
-      setDate(resp.data.date) 
-    }
-    fetchDate()
-  }, [])
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to React.js.<br/><br/>
-          Today's Date is {date}
+          Welcome to GTS front-end<br/><br/>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
